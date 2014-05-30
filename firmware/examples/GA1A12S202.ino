d3ed2d4e8de4b8b0763f190ea075cbf6a9da9464
Garrett Bartley <wgbartley@gmail.com>
2014-05-30 16:43:46 -0400

    fixed spacing in example

--- firmware/examples/GA1A12S202.ino
+++ firmware/examples/GA1A12S202.ino
@@ -1,12 +1,12 @@
-
 #include "GA1A12S202/GA1A12S202.h"
 
-GA1A12S202 lux(A0); void setup() {
+GA1A12S202 lux(A0);
+
+void setup() {
     Serial.begin(115200);
     delay(5000);
-    
-    
 }
+
 void loop() {
     Serial.print("Raw = ");
     Serial.print(lux.getRaw());
